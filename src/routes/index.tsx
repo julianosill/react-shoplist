8b5a1ef30c5f72f5cc582c98161b1238021ddef4
Juliano Sill <sill.juliano@gmail.com>
2023-09-21 16:40:21 -0300

    initial commit

---
+++ src/routes/index.tsx
@@ -0,0 +1,11 @@
+import { Routes, Route } from 'react-router-dom'
+
+import Login from '../pages/Login'
+
+export default function RoutesApp() {
+  return (
+    <Routes>
+      <Route path="/" element={<Login />} />
+    </Routes>
+  )
+}
