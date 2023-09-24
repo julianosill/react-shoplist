392ca6352e00f1001fd99cdc83d44724ac71a657
Juliano Sill <sill.juliano@gmail.com>
2023-09-24 13:45:45 -0300

    fix(Button): center content

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -5,18 +5,11 @@ export type ButtonProps = ComponentProps<'button'> & {
   hasIcon?: boolean
 }
 
-export function Button({
-  width = 'w-fit',
-  hasIcon = false,
-  ...props
-}: ButtonProps) {
+export function Button({ width = 'w-fit', ...props }: ButtonProps) {
   const disabledClasses = 'disabled:opacity-50 disabled:hover:bg-teal-500'
-  const iconClasses = 'flex items-center gap-3'
   return (
     <button
-      className={`${width} h-12 px-12 rounded-md bg-teal-500 text-white hover:bg-teal-600 focus:outline-teal-500 outline-offset-4 transition ease-in-out ${disabledClasses} ${
-        hasIcon && iconClasses
-      }`}
+      className={`${width} h-12 px-12 flex justify-center items-center gap-3 rounded-md bg-teal-500 text-white hover:bg-teal-600 focus:outline-teal-500 outline-offset-4 transition ease-in-out ${disabledClasses}`}
       {...props}
     />
   )
