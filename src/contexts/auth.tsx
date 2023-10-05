c79fe6ede7655c635f667098e2208ba32d7bdba6
Juliano Sill <sill.juliano@gmail.com>
2023-10-05 14:22:08 -0300

    feat(login): update login function

--- src/contexts/auth.tsx
+++ src/contexts/auth.tsx
@@ -1,6 +1,7 @@
 import { createContext, useState } from 'react'
 import { auth } from '../services/firebaseConnection'
-import { signInWithEmailAndPassword } from 'firebase/auth'
+import { signInWithEmailAndPassword, signOut } from 'firebase/auth'
+import { useNavigate } from 'react-router-dom'
 
 type AuthProviderProps = {
   children: React.ReactNode
@@ -9,6 +10,11 @@ type AuthProviderProps = {
 type User = {
   email: string
   password: string
+}
+
+type LocalUser = {
+  uid: string
+  email: string | null
 }
 
 type AuthError = {
@@ -21,6 +27,7 @@ type AuthContextType = {
   user: User
   setUser: React.Dispatch<React.SetStateAction<User>>
   login: () => Promise<void>
+  logout: () => Promise<void>
   loadingAuth: boolean
   authError: AuthError
   setAuthError: React.Dispatch<React.SetStateAction<AuthError>>
@@ -30,22 +37,30 @@ export const AuthContext = createContext<AuthContextType>({
   user: { email: '', password: '' },
   setUser: () => {},
   login: async () => {},
+  logout: async () => {},
   loadingAuth: false,
   authError: {},
   setAuthError: () => {},
 })
 
 export default function AuthProvider({ children }: AuthProviderProps) {
+  const [loading, setLoading] = useState(true)
   const [user, setUser] = useState({ email: '', password: '' })
   const [loadingAuth, setLoadingAuth] = useState(false)
   const [authError, setAuthError] = useState({})
+  const navigate = useNavigate()
 
   async function login() {
     setLoadingAuth(true)
     setAuthError({})
     await signInWithEmailAndPassword(auth, user.email, user.password)
-      .then(() => {
-        alert('Logged in!')
+      .then(async (value) => {
+        const userData = {
+          uid: value.user.uid,
+          email: value.user.email,
+        }
+        storageUser(userData)
+        navigate('/list')
       })
       .catch((error) => handleAuthError(error.code))
       .finally(() => {
@@ -85,6 +100,19 @@ export default function AuthProvider({ children }: AuthProviderProps) {
     }
   }
 
+  async function logout() {
+    await signOut(auth)
+      .then(() => {
+        setUser({ email: '', password: '' })
+        localStorage.removeItem('@shoplist')
+      })
+      .catch((error) => console.log(error))
+  }
+
+  const storageUser = (user: LocalUser) => {
+    localStorage.setItem('@shoplist', JSON.stringify(user))
+  }
+
   return (
     <AuthContext.Provider
       value={{
@@ -94,6 +122,7 @@ export default function AuthProvider({ children }: AuthProviderProps) {
         authError,
         setAuthError,
         login,
+        logout,
       }}
     >
       {children}
