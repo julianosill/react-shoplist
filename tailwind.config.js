696e9875f6d48c8f03d168d1eb3dd2bcb7983031
Juliano Sill <sill.juliano@gmail.com>
2023-10-06 14:41:30 -0300

    build: include google font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,30 +1,13 @@
 /** @type {import('tailwindcss').Config} */
+import { fontFamily as defaultFontFamily } from 'tailwindcss/defaultTheme'
+const fontFamily = defaultFontFamily
+
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    container: {
-      center: true,
-      padding: '2rem',
-    },
     extend: {
       fontFamily: {
-        sans: [
-          'Inter',
-          'ui-sans-serif',
-          'system-ui',
-          '-apple-system',
-          'BlinkMacSystemFont',
-          'Segoe UI',
-          'Roboto',
-          'Helvetica Neue',
-          'Arial',
-          'Noto Sans',
-          'sans-serif',
-          'Apple Color Emoji',
-          'Segoe UI Emoji',
-          'Segoe UI Symbol',
-          'Noto Color Emoji',
-        ],
+        sans: ['Inter', ...fontFamily.sans],
       },
     },
   },
